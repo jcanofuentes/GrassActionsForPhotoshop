5768a1ab7f122d9f860edb414ca84eac90e1dfae
jcanofuentes <jcanofuentes@gmail.com>
2023-01-03 15:08:06 +0100

    Update GrassActionsForPhotoshop_Installer.bat

--- GrassActionsForPhotoshop_Installer.bat
+++ GrassActionsForPhotoshop_Installer.bat
@@ -1,87 +1,98 @@
 @echo off
-setlocal
 
-REM Store current path
-set source_path=%~dp0
-echo "SOURCE PATH:" %source_path%
+goto :Main
 
-REM Check for Admin privileges
-NET SESSION >nul 2>&1
-IF %ERRORLEVEL% EQU 0 (
-    ECHO Administrator PRIVILEGES Detected! 
-) ELSE (
-    echo msgbox "ERROR: Launch this program as an administrator by right-clicking on the executable file and choosing 'Run as administrator.'" > %tmp%\tmp.vbs
-	cscript /nologo %tmp%\tmp.vbs
-	del %tmp%\tmp.vbs
+::----------------------------------------------------------------------
+::-- Main
+::--
+::-- param:   none
+::-- return:  none
+::----------------------------------------------------------------------
+:Main
+	set sourcePath=%~dp0
+	set tmpVBSFile=%sourcePath%\tmp.vbs
+	set outputResult=Empty
+
+	call:checkPermissions		
+	
+	call:PrintWithVBS "Welcome to the 'SENECA extensions for Photoshop' setup. First, you have to select the GRASS installation folder"
+	
+	set "psCommand="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Usual path is: C:\Program Files\GRASS GIS X.X',0,0).self.path""
+	for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
+	
+	REM Check for grass*.bat 
+	setlocal enabledelayedexpansion
+	cd /D !folder!
+	if exist "grass*.bat" (
+		call:PrintWithVBS  "Your GRASS installation is at: !folder!"
+	) else (
+		call:PrintWithVBS "Grass is not found, exitting setup"
+		timeout 3
+		exit
+	)
+	
+	REM Create an environment variable for later use
+	call:PrintWithVBS "New environment variable added: SENECA_GRASS_FOLDER"
+	setx -m SENECA_GRASS_FOLDER "!folder!"
+	setx env SENECA_GRASS_FOLDER /M
+
+	REM Ask the user for the 'SENECA temporal folder' place
+	call:PrintWithVBS "This extension needs a folder where the necessary files will be save to allow communication between Photoshop and GRASS. Please select the drive where you want this folder to be created (an SSD drive is preferable)"
+	set "psCommand="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Select drive...',0,0).self.path""
+	for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder_2=%%I"
+	setlocal enabledelayedexpansion
+
+	REM Actually creates the folder
+	cd /D %folder_2%
+	mkdir "SENECA_TEMP_FOLDER"
+	cd "SENECA_TEMP_FOLDER"
+	setx -m SENECA_TEMP_FOLDER "%cd%"
+	setx env SENECA_TEMP_FOLDER /M
+
+	REM Copy files to temp folder
+	xcopy /s/z "%source_path%\grass_scripts" "%cd%"
+
+
+	REM Last questions...
+	call:PrintWithVBS "Don't forget to copy all jsx files to Photshop script folder (usually at: C:\Program Files\Adobe\Adobe Photoshop XXXX\Presets\Scripts)"
+	call:PrintWithVBS "Finally, don't forget to create a Grass Location and update the file 'shade.bat' with the correct one"
+
+	timeout 3
 	exit
-)
+	
+::----------------------------------------------------------------------
+::-- checkPermissions
+::-- Check for admin privileges. Stop script execution if no admin 
+::-- rights
+::--
+::-- param:   none
+::-- return:  none
+::----------------------------------------------------------------------
 
-REM Installation info...
-echo msgbox "Welcome to the 'SENECA extensions for Photoshop' setup" > %tmp%\tmp.vbs
-cscript /nologo %tmp%\tmp.vbs
-del %tmp%\tmp.vbs
-
-REM More info...
-echo msgbox "First, you have to select the GRASS installation folder" > %tmp%\tmp.vbs
-cscript /nologo %tmp%\tmp.vbs
-del %tmp%\tmp.vbs
-
-REM Ask the user for the GRASS installation folder
-set "psCommand="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Usual path is: C:\Program Files\GRASS GIS 7.X',0,0).self.path""
-for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder=%%I"
-
-setlocal enabledelayedexpansion
-
-REM Check for grass76.bat 
-cd /D !folder!
-if exist "grass*.bat" (
-	echo msgbox "Your GRASS installation is at: !folder!" > %tmp%\tmp.vbs
-	cscript /nologo %tmp%\tmp.vbs
-	del %tmp%\tmp.vbs
-) else (
-	REM Grass not found...exitting
-	echo msgbox "Grass is not found, exitting setup" > %tmp%\tmp.vbs
-	cscript /nologo %tmp%\tmp.vbs
-	del %tmp%\tmp.vbs
-	exit
-)
-
-REM Create an environment variable for later use
-echo msgbox "New environment variable added: SENECA_GRASS_FOLDER" > %tmp%\tmp.vbs
-cscript /nologo %tmp%\tmp.vbs
-del %tmp%\tmp.vbs
-setx -m SENECA_GRASS_FOLDER "!folder!"
-setx env SENECA_GRASS_FOLDER /M
-
-REM Ask the user for the 'SENECA temporal folder' place
-echo msgbox "This extension needs a folder where the necessary files will be save to allow communication between Photoshop and GRASS. Please select the drive where you want this folder to be created (an SSD drive is preferable)" > %tmp%\tmp.vbs
-cscript /nologo %tmp%\tmp.vbs
-del %tmp%\tmp.vbs
-set "psCommand="(new-object -COM 'Shell.Application').BrowseForFolder(0,'Select drive...',0,0).self.path""
-for /f "usebackq delims=" %%I in (`powershell %psCommand%`) do set "folder_2=%%I"
-setlocal enabledelayedexpansion
-
-REM Actually creates the folder
-cd /D %folder_2%
-mkdir "SENECA_TEMP_FOLDER"
-cd "SENECA_TEMP_FOLDER"
-setx -m SENECA_TEMP_FOLDER "%cd%"
-setx env SENECA_TEMP_FOLDER /M
-
-REM Copy files to temp folder
-xcopy /s/z "%source_path%\grass_scripts" "%cd%"
-
-
-REM Last questions...
-echo msgbox "Don't forget to copy the Render_32bits.jsx file to C:\Program Files\Adobe\Adobe Photoshop XXXX\Presets\Scripts" > %tmp%\tmp.vbs
-cscript /nologo %tmp%\tmp.vbs
-del %tmp%\tmp.vbs
-
-echo msgbox "Finally, don't forget to create a Grass Location and update the file 'shade.bat' with the correct one" > %tmp%\tmp.vbs
-cscript /nologo %tmp%\tmp.vbs
-del %tmp%\tmp.vbs
-
-pause 10
+:checkPermissions
+    echo Administrative permissions required. Detecting permissions...
+    
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        echo Administrator privileges detected, the installation continues.
+    ) else (
+        call:PrintWithVBS "ERROR: Launch this program as an administrator by right-clicking on the executable file and choosing 'Run as administrator.'"
+		timeout 3
+		exit
+    )
+    
+	goto :eof
+	
+::----------------------------------------------------------------------
+::-- PrintWithVBS
+::-- Shows a msgbox using VBS
+::--
+::-- param: the message inbetween double quotes
+::----------------------------------------------------------------------
+:PrintWithVBS
+	echo msgbox "%~1" > %tmpVBSFile%
+	cscript /nologo %tmpVBSFile%
+	goto :eof
 
 REM Recent changes: 
 REM 03/01/2023 Now it works for both Grass 7.6 and Grass 7.8
